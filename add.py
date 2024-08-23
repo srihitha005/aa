c5a5782c7f0b0a33be7e974b50ebe51ec59cdd57
srihitha005 <160151561+srihitha005@users.noreply.github.com>
2024-08-23 10:52:26 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    import sys
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
